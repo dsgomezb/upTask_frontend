c4fafb3e53c832f55c04c7b0c66643dbc3f256c8
Daniel Stiven Gomez Benavides <daniel.gomez@datatools.com.co>
2024-11-06 10:27:07 -0500

    Se actualiza nombre de etiquetas

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
+import { Popover, PopoverButton, PopoverPanel, Transition } from '@headlessui/react';
 import { Bars3Icon } from '@heroicons/react/20/solid';
 import { Link, useNavigate } from 'react-router-dom';
 import { User } from '../types';
@@ -18,9 +18,9 @@ export default function NavMenu({ name }: NavMenuProps) {
   };
   return (
     <Popover className="relative">
-      <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
+      <PopoverButton className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 p-1 rounded-lg bg-purple-400">
         <Bars3Icon className="w-8 h-8 text-white " />
-      </Popover.Button>
+      </PopoverButton>
 
       <Transition
         as={Fragment}
@@ -31,7 +31,7 @@ export default function NavMenu({ name }: NavMenuProps) {
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
+        <PopoverPanel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
           <div className="w-full lg:w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
             <p className="text-center">Hello: {name}</p>
             <Link to="/profile" className="block p-2 hover:text-purple-950">
@@ -44,7 +44,7 @@ export default function NavMenu({ name }: NavMenuProps) {
               Logout
             </button>
           </div>
-        </Popover.Panel>
+        </PopoverPanel>
       </Transition>
     </Popover>
   );
